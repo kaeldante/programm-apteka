fbaa7ce2a3e8c6544046ba00298f3edc6b236268
Danil Yavorskiy <63715687+kaeldante@users.noreply.github.com>
2021-01-16 10:31:03 +0600

    Add files via upload

---
+++ test/test/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: 123
+ * Дата: 17.11.2019
+ * Время: 17:00
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+using System;
+
+namespace test
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int num=3;
+			num--;
+			Console.WriteLine(num);
+			Console.ReadKey();
+		}
+	}
+}
